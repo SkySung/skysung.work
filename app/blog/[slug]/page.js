438937db5103fca4774b754604867bfb20783e74
SkySung <skysung0217@gmail.com>
2024-11-28 17:44:13 +0800

    新增 Markdown 內容轉換功能，更新文章樣式並整合 Tailwind Typography 插件

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -6,10 +6,10 @@ export default async function BlogPost({ params }) {
   const post = await getPostBySlug(params.slug);
 
   return (
-    <article className="max-w-2xl mx-auto">
+    <article className="dark:prose-invert prose-lg max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
       <p className="text-gray-600 mb-8">{post.date}</p>
-      <div className="prose prose-lg">
+      <div className="">
         <MDXRemote source={post.content} />
       </div>
     </article>
